cb3bbe7cb9ec20ae1a153779f4c8b6c53496d0cd
KristiSeraj <serajkristi@gmail.com>
2023-09-13 16:37:36 +0200

    removed uneccessary components

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import Navbar from '../components/Navbar';
 import NewTask from '../components/NewTask';
 import Tasks from '../components/Tasks';
 
@@ -15,7 +14,6 @@ const Home = () => {
     }
     return (
         <>
-            {/* <Navbar /> */}
             <div className='ml-[50%]'>
                 <button onClick={handleOpenModal} className='bg-gray-600 hover:bg-gray-700 text-white font-bold py-2 px-4 my-4 border border-gray-700 rounded'>Add task</button>
             </div>
